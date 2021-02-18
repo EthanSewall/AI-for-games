f2bac80c67d5b28444921bfc6e56d160e810d43d
Ethan <lemonmitts@gmail.com>
2021-02-18 12:45:32 -0800

    initial commit

---
+++ Assets/AIcontroller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIcontroller : MonoBehaviour
+{
+    Agent agent;
+    public enum Behaviors { Seek, Flee}
+    List<Behaviors> currentBehaviors;
+    public Transform target;
+    public float speed;
+    public float maxForce;
+    public bool addBehavior;
+    public Behaviors addingBehavior;
+
+    void Start()
+    {
+        currentBehaviors = new List<Behaviors>();
+        agent = gameObject.GetComponent<Agent>();
+    }
+
+
+    void Update()
+    {
+        if(addBehavior)
+        {
+            currentBehaviors.Add(addingBehavior);
+            addBehavior = false;
+        }
+
+        Vector3 steering = Vector3.zero;
+        foreach (Behaviors b in currentBehaviors)
+        {
+            switch (b)
+            {
+                case Behaviors.Seek:
+                    {
+                        Vector3 seek = (target.position - gameObject.transform.position).normalized * speed;
+                        steering += seek;
+                    }
+                    break;
+                case Behaviors.Flee:
+                    {
+                        Vector3 flee = (target.position - gameObject.transform.position).normalized * speed;
+                        steering -= flee;
+                    }
+                    break;
+            }
+        }
+        steering = Vector3.ClampMagnitude(steering, maxForce);
+        agent.UpdateMovement(steering);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Agent agent;
+    public float speed;
+    void Start()
+    {
+        agent = gameObject.GetComponent<Agent>();
+    }
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1);
+        agent.UpdateMovement(input * speed);
+    }
+}
